fcd996bdfb9befe92ab2225f2be079831967237b
kkkuro90 <114396499+kkkuro90@users.noreply.github.com>
2025-02-20 00:06:16 +0500

    Create InfoDTO.php

---
+++ app/DTOs/InfoDTO.php
@@ -0,0 +1,17 @@
+namespace App\DTOs;
+
+class InfoDTO {
+    public $phpVersion;
+    public $serverSoftware;
+    public $clientIp;
+    public $clientUser Agent;
+    public $database;
+
+    public function __construct($phpVersion, $serverSoftware, $clientIp, $clientUser Agent, $database) {
+        $this->phpVersion = $phpVersion;
+        $this->serverSoftware = $serverSoftware;
+        $this->clientIp = $clientIp;
+        $this->clientUser Agent = $clientUser Agent;
+        $this->database = $database;
+    }
+}

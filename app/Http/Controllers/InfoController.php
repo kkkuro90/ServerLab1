7a29686791209cdd260800d75472aab76d4b7081
kkkuro90 <114396499+kkkuro90@users.noreply.github.com>
2025-02-20 00:00:46 +0500

    Update InfoController.php methods

--- app/Http/Controllers/InfoController.php
+++ app/Http/Controllers/InfoController.php
@@ -6,5 +6,24 @@ use Illuminate\Http\Request;
 
 class InfoController extends Controller
 {
-    //
+    public function serverInfo() {
+    return response()->json([
+        'php_version' => phpversion(),
+        'server_software' => $_SERVER['SERVER_SOFTWARE'],
+    ]);
+}
+
+public function clientInfo() {
+    return response()->json([
+        'ip' => request()->ip(),
+        'user_agent' => request()->userAgent(),
+    ]);
+}
+
+public function databaseInfo() {
+    $database = env('DB_DATABASE');
+    return response()->json([
+        'database' => $database,
+    ]);
+}
 }
